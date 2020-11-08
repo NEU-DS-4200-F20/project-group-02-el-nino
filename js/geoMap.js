5f0ad41dcaff7a49b6f91acf10cc242bb47dc3ee
Logan Jones <loganjones@Logans-Air.fios-router.home>
2020-11-08 15:00:03 -0500

    fill by sst

--- js/geoMap.js
+++ js/geoMap.js
@@ -1,7 +1,9 @@
 import usa from "../data/usa.js";
 
 function geoMap() {
-  const projection = d3.geoEquirectangular();
+  const projection = d3.geoEquirectangular()
+    //.scale(1000)
+    .center([-70, 42.313]);
 
   const geoGenerator = d3.geoPath().projection(projection);
 
@@ -12,6 +14,27 @@ function geoMap() {
 
   // Create path elements and update the d attribute using the geo generator
   u.enter().append("path").attr("d", geoGenerator);
+
+  var myColor = d3.scaleLinear().domain([-2,32])
+    .range(["white", "blue"])
+
+  d3.csv("data/sst_small.csv").then(function(data) {
+    svg.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("circle")
+       .attr("cx", function(d) {
+               return projection([d.longitude, d.latitude])[0];
+       })
+       .attr("cy", function(d) {
+               return projection([d.longitude, d.latitude])[1];
+       })
+       .attr("r", 5)
+       .attr("fill", function(d) {
+         return myColor(d.sst) 
+       });
+  });
+
 }
 
 export default geoMap;
