638870762dc3e3bceba97d859ba8b5e29b9b299f
Alexander <schad.a@northeastern.edu>
2020-11-16 16:08:54 -0500

    Added ONI Index and Other Line chart

--- js/lineChart.js
+++ js/lineChart.js
@@ -1,59 +1,233 @@
-var parseDate = d3.timeParse('%m/%d/%Y');
+var formatTime = d3.timeFormat("%Y - %m");
 
-d3.csv('data/discharge.csv').then(lineChart);
 
-function lineChart(data){
-    console.log(data);
 
-    var maxDate  = d3.max(data, function(d){return d.date; });
-    var minDate  = d3.min(data, function(d){return d.date; });
-    var maxDischarge = d3.max(data, function(d){return d.discharge;});
+function lineChart(condition) {
+
+    switch (condition) {
+        case ("precip") :
+            precip();
+            break;
+        case ("soil") :
+            soil();
+            break;
+        case ("discharge") :
+            discharge();
+            break;
+        default :
+            discharge();
+            break;
+    }
+}
+
+function precip() {
+    d3.csv("data/precip.csv").then(function (data) {
+
+        var maxDate  = d3.max(data, function(d){return Date.parse(d.date); });
+        var minDate  = d3.min(data, function(d){return Date.parse(d.date); });
+        var maxVar = d3.max(data, function(d){return parseFloat(d.precip)});
+        console.log(maxDate, minDate, maxVar);
+    
+        var width  = 1200;
+        var height = 600;
+        var margin = {
+            top: 30,
+            bottom: 30,
+            left: 70,
+            right: 100
+        };
+    
+        var svg = d3.select('#linechart')
+            .append('svg')
+                .attr('width' , width)
+                .attr('height', height)
+                .style('background', '#efefef')
+            .append('g')
+                .attr('transform','translate(' + margin.left +',' + margin.top + ')');
+    
+        var xScale = d3.scaleTime()
+            .domain([minDate, maxDate])
+            .range([0, width - margin.left - margin.right]);
+    
+        var yScale = d3.scaleLinear()
+            .domain([0, maxVar])
+            .range([height - margin.bottom - margin.top, 0]);
+    
+        var xAxis = d3.axisBottom(xScale);
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
+            .call(xAxis);
+    
+        var yAxis = d3.axisLeft(yScale);
+        svg.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate(0, 0)')
+            .call(yAxis);
+    
+    
+       svg.append('path')
+        .datum(data)
+        .attr('class', 'dateLine')
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+          .attr('d', d3.line()
+          .x(function(d){return xScale(Date.parse(d.date));})
+          .y(function(d){return yScale(d.precip);})
+        );
+    
+    // Add the points
+    let points = svg.append('g')
+      .selectAll('.linePoint')
+        .data(data);
+    
+    
+    });
+        return lineChart;
+    
+}
+
+function soil() {
+    d3.csv("data/soilm.csv").then(function (data) {
+
+        var maxDate  = d3.max(data, function(d){return Date.parse(d.date); });
+        var minDate  = d3.min(data, function(d){return Date.parse(d.date); });
+        var maxVar = d3.max(data, function(d){return parseFloat(d.soilm)});
+        console.log(maxDate, minDate, maxVar);
+    
+        var width  = 1200;
+        var height = 600;
+        var margin = {
+            top: 30,
+            bottom: 30,
+            left: 70,
+            right: 100
+        };
+    
+        var svg = d3.select('#linechart')
+            .append('svg')
+                .attr('width' , width)
+                .attr('height', height)
+                .style('background', '#efefef')
+            .append('g')
+                .attr('transform','translate(' + margin.left +',' + margin.top + ')');
+    
+        var xScale = d3.scaleTime()
+            .domain([minDate, maxDate])
+            .range([0, width - margin.left - margin.right]);
+    
+        var yScale = d3.scaleLinear()
+            .domain([0, maxVar])
+            .range([height - margin.bottom - margin.top, 0]);
+    
+        var xAxis = d3.axisBottom(xScale);
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
+            .call(xAxis);
+    
+        var yAxis = d3.axisLeft(yScale);
+        svg.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate(0, 0)')
+            .call(yAxis);
+    
+    
+       svg.append('path')
+        .datum(data)
+        .attr('class', 'dateLine')
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+          .attr('d', d3.line()
+          .x(function(d){return xScale(Date.parse(d.date));})
+          .y(function(d){return yScale(d.soilm);})
+        );
+    
+    // Add the points
+    let points = svg.append('g')
+      .selectAll('.linePoint')
+        .data(data);
+    
+    
+    });
+        return lineChart;
+    
+    }
+
+
+function discharge() {
+    d3.csv("data/discharge.csv").then(function (data) {
+
+    var maxDate  = d3.max(data, function(d){return Date.parse(d.date); });
+    var minDate  = d3.min(data, function(d){return Date.parse(d.date); });
+    var maxDischarge = d3.max(data, function(d){return parseInt(d.discharge)});
     console.log(maxDate, minDate, maxDischarge);
 
-    var width  = 600;
-    var height = 500;
+    var width  = 1200;
+    var height = 600;
     var margin = {
         top: 30,
         bottom: 30,
-        left: 30,
-        right: 30
+        left: 70,
+        right: 100
     };
 
-    var svg = d3.select('#vis-svg-1')
+    var svg = d3.select('#linechart')
         .append('svg')
             .attr('width' , width)
             .attr('height', height)
-            .style('background', '#efefef');
-
-    var chartGroup = svg
+            .style('background', '#efefef')
         .append('g')
             .attr('transform','translate(' + margin.left +',' + margin.top + ')');
 
     var xScale = d3.scaleTime()
         .domain([minDate, maxDate])
-        .range([0, width]);
+        .range([0, width - margin.left - margin.right]);
 
     var yScale = d3.scaleLinear()
         .domain([0, maxDischarge])
         .range([height - margin.bottom - margin.top, 0]);
 
     var xAxis = d3.axisBottom(xScale);
-    chartGroup.append('g')
+    svg.append('g')
         .attr('class', 'x axis')
         .attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
         .call(xAxis);
 
     var yAxis = d3.axisLeft(yScale);
-    chartGroup.append('g')
+    svg.append('g')
         .attr('class', 'y axis')
         .attr('transform', 'translate(0, 0)')
         .call(yAxis);
 
-    var line = d3.line()
-    .x(function(d){return xScale(d.date);})    
-    .y(function(d){return yScale(d.discharge);})
 
-    chartGroup.append('path')
-        .attr('d', line(data))
-        .attr('class', 'dataLine');
+   svg.append('path')
+    .datum(data)
+    .attr('class', 'dateLine')
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+      .attr('d', d3.line()
+      .x(function(d){return xScale(Date.parse(d.date));})
+      .y(function(d){return yScale(d.discharge);})
+    );
+
+    // Add the points
+    svg.selectAll("myCircles")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("fill", "black")
+      .attr("stroke", "none")
+      .attr("cx", function(d) { return xScale(Date.parse(d.date)) })
+      .attr("cy", function(d) { return yScale(d.discharge) })
+      .attr("r", 2)
+
+
+});
+    return lineChart;
+
 }
+
--- js/visualization.js
+++ js/visualization.js
@@ -1,5 +1,11 @@
 // Immediately Invoked Function Expression to limit access to our
 // variables and prevent
 (() => {
+
+  //mrbMap("percip");
   sstMap();
+  oni();
+  //lineChart("soil");
+  //lineChart("precip");
+  lineChart("discharge");
 })();
